8fd12796b13eda6a5865c2b2ecc274610ba7f888
Vladislav <vladonxp@mail.ru>
2017-02-06 17:38:39 +0500

    adding jwt support

--- app/index.jsx
+++ app/index.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
+import jwt from 'jwt-simple'
 
 // components
 import App from './containers/App'
@@ -23,7 +24,10 @@ import defaultState from './store/defaultState'
 
 const store = configureStore(JSON.parse(localStorage['fit.root'] || JSON.stringify(defaultState)));
 
-console.log(store.getState());
+const str = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2dyb3VwX2lkIjoiQWxleGFuZGVyIiwic3ViIjozLCJpc3MiOiJodHRwOlwvXC9maXQubG9jYWxcL2FwaVwvYXV0aFwvbG9naW4iLCJpYXQiOjE0ODYzODQ1MjIsImV4cCI6MTQ4NjM4ODEyMiwibmJmIjoxNDg2Mzg0NTIyLCJqdGkiOiIwY2IyZTU1YTQxNTU4MWNkODMwOTJhYzhiZjhjZTg1ZCJ9.OrHB40RZDBeJRcd3sfzuYPOaj2Zg6CiZFxeIrBBEtjg';
+const secret = 'privetmedved';
+
+var token = jwt.decode(str, secret);
 
 render( 
 		<Provider store={store}>
